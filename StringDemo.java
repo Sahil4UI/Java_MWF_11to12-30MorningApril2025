fc4436fff737d18baa6e3f1091f0e026fffdbc90
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-09 11:26:37 +0530

    Create StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,56 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		String x = "hello Welcome to java";
+//		System.out.println(x);
+//		indexing
+//		System.out.println(x.charAt(3));
+//		enhanced for Loop
+//		for (char value : x.toCharArray())
+//		{
+//				System.out.println(value);
+//		}
+//		System.out.println(x.length());
+		
+		
+//		System.out.println(a+b+98+98.678);
+		
+		String a = "hello world world world";
+		String b = "hello";
+//		String b = new String ("hello");
+				
+//		System.out.println(a==b);
+//		System.out.println(a.equals(b));
+//		System.out.println(a.toUpperCase());
+//		System.out.println(a.toLowerCase());
+//		System.out.println(a.toCharArray());
+//		System.out.println(a.codePointAt(0));
+//		-ve if string < string2
+//		+ve if string> string2
+//		0 if string==string2
+//		System.out.println(a.compareTo(b));
+//		System.out.println(a.replaceAll("world", "WORLD"));
+//		System.out.println(a.replaceFirst("world", "WORLD"));	
+//		System.out.println(a.substring(0,5));
+//		ending - (n-1)
+		
+//		Write a program to find length of string 
+//		without using length() method
+		
+// find the reverse of String
+//		abcde - edcba
+		
+// find the occurence of character in string
+//		hello - l - 2
+		
+// remove duplicates from string
+//		aabbccddeeee - abcde
+		
+		
+
+	}
+
+}
